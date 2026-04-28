01f44ba6efcb88bf8a649f69ef02b23f38879144
Elynzx <rpascual06@outlook.com>
2026-04-27 22:40:42 -0500

    Refactor layout

--- src/layouts/root-layout.jsx
+++ src/layouts/root-layout.jsx
@@ -3,11 +3,11 @@ import { Header } from "../components/header/header";
 
 export function RootLayout() {
     return (
-        <div className="flex items-center justify-center h-screen w-full font-[Karla] bg-linear-to-b from-bgPink to-bgGreen overflow-hidden p-5 md:p-0">
-            <div className="relative w-full max-w-7xl h-full md:h-[90vh] flex flex-col">
+        <div className="flex items-center justify-center h-dvh w-full font-[Karla] bg-linear-to-b from-bgPink to-bgGreen overflow-hidden">
+            <div className="relative w-full max-w-7xl h-full flex flex-col p-4 md:py-12">
                 <Header />
-                <main className="flex-1 flex flex-col min-h-0 w-full mt-6 md:mt-12 ">
-                    <div className="flex-1 flex flex-col p-3 md:p-6 rounded-3xl bg-white/30 backdrop-blur-xl border-4 border-white/40 shadow-2xl relative overflow-hidden">
+                <main className="flex-1 flex flex-col min-h-0 w-full mt-8 md:mt-10">
+                    <div className="flex-1 flex flex-col p-3 md:p-6 rounded-3xl bg-white/30 backdrop-blur-xl border-4 border-white/40 shadow-2xl relative min-h-0">
                         <div className="relative mt-4 flex-1 flex flex-col rounded-2xl border-2 border-bgDarkGray bg-white/10 shadow-inner overflow-y-auto scrollbar-hide">
                             <Outlet />
                         </div>
