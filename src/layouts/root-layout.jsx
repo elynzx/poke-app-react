9fb0bb70280491eb9d9b141ef65425ec89ea626b
Elynzx <rpascual06@outlook.com>
2026-04-27 20:09:00 -0500

    Adjust margins for improved layout in RootLayout component

--- src/layouts/root-layout.jsx
+++ src/layouts/root-layout.jsx
@@ -7,9 +7,9 @@ export function RootLayout() {
         <div className="flex items-center justify-center min-h-screen w-full font-[Karla] bg-linear-to-b from-bgPink to-bgGreen md:px-6 px-5">
             <div className="relative w-full max-w-300 h-[92vh] md:h-[90vh] flex flex-col">
                 <Header />
-                <main className="flex-1 flex flex-col min-h-0 w-full mt-10 md:mt-16">
+                <main className="flex-1 flex flex-col min-h-0 w-full mt-6 md:mt-12">
                     <div className="flex-1 flex flex-col p-3 md:p-6 rounded-3xl bg-white/30 backdrop-blur-xl border-4 border-white/40 shadow-2xl relative overflow-hidden">
-                        <div className="relative mt-4 md:mt-0 flex-1 flex flex-col rounded-2xl border-2 border-bgDarkGray bg-white/10 shadow-inner overflow-y-auto scrollbar-hide">
+                        <div className="relative mt-4 flex-1 flex flex-col rounded-2xl border-2 border-bgDarkGray bg-white/10 shadow-inner overflow-y-auto scrollbar-hide">
                             <Outlet />
                         </div>
                     </div>
