ecc95967bd98bc3691289f49c67e1786378810de
Elynzx <rpascual06@outlook.com>
2026-04-28 14:54:31 -0500

    Make PokemonCard clickable to navigate to individual Pokemon details

--- src/components/pokemon-card/pokemon-card.jsx
+++ src/components/pokemon-card/pokemon-card.jsx
@@ -12,7 +12,10 @@ export const PokemonCard = ({ pokemon }) => {
     const formattedId = `#${id.toString().padStart(3, "0")}`;
 
     return (
-        <div className="relative w-42 h-60 flex flex-col items-center justify-end group">
+        <div
+            onClick={() => navigate(`/pokemons/${pokemon.name}`)}
+            className="relative w-42 h-60 flex flex-col items-center justify-end group"
+        >
             <div className="absolute -top-3 z-10 w-full h-38 flex justify-center items-center pointer-events-none transition-transform duration-300 group-hover:scale-110">
                 <img
                     src={image}
@@ -20,11 +23,7 @@ export const PokemonCard = ({ pokemon }) => {
                     className="h-full object-contain drop-shadow-[0_6px_7px_rgba(0,0,0,0.3)]"
                 />
             </div>
-
-            <div
-                onClick={() => navigate(`/pokemons/${pokemon.name}`)}
-                className="relative w-full h-35 pb-6 flex flex-col items-center justify-end bg-white/20 backdrop-blur-md border border-white/50 rounded-b-xl  rounded-t-4xl shadow-2xs shadow-white transition-all duration-300 group-hover:bg-white/40 cursor-pointer"
-            >
+            <div className="relative w-full h-35 pb-6 flex flex-col items-center justify-end bg-white/20 backdrop-blur-md border border-white/50 rounded-b-xl  rounded-t-4xl shadow-2xs shadow-white transition-all duration-300 group-hover:bg-white/40 cursor-pointer">
                 <div className="flex flex-col items-center text-bgDarkGray text-center px-2">
                     <span className="text-[10px] font-bold opacity-50">
                         {formattedId}
