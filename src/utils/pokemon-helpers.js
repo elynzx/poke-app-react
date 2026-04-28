f88ec4e3fe90b1447ce1096483de9b8d3a5ae898
Elynzx <rpascual06@outlook.com>
2026-04-27 20:47:06 -0500

    Refactor PokemonProfile  image selection logic

--- src/utils/pokemon-helpers.js
+++ src/utils/pokemon-helpers.js
@@ -4,13 +4,6 @@ export const createPokemonProfile = (pokemon) => ({
     image:
         pokemon.sprites.other["home"].front_default ||
         pokemon.sprites.front_default,
-
-    /*     image:
-        pokemon.sprites?.versions?.["generation-v"]?.["black-white"]?.animated
-            ?.front_default ||
-        pokemon.sprites?.other?.dream_world?.front_default ||
-        pokemon.sprites?.other?.["official-artwork"]?.front_default ||
-        pokemon.sprites?.front_default, */
     weight: pokemon.weight,
     height: pokemon.height,
     types: pokemon.types?.map((type) => type.type.name),
