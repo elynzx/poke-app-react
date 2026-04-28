359ff904522d45a373e99274d5ebce56a9967858
Elynzx <rpascual06@outlook.com>
2026-04-28 14:56:19 -0500

    Add Compare and Favorites pages with basic structure

---
+++ src/pages/compare.jsx
@@ -0,0 +1,8 @@
+import { CompareCard } from "../components/compare-card/compare-card";
+
+export function ComparePage() {
+    return (
+        <div className="flex flex-col lg:flex-row h-full w-full items-center justify-center gap-8 lg:gap-16 p-6 overflow-y-auto scrollbar-hide bg-white/20">
+        </div>
+    );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,7 @@
 export function HomePage() {
-    return <>holie</>;
+    return(
+        <div>
+            
+        </div>
+    )
 }
