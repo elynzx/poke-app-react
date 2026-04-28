fac68f7e8b779071851d000e6f3d18ea00e9bdb1
Elynzx <rpascual06@outlook.com>
2026-04-27 21:56:38 -0500

    Enhance Header component with responsive menu and improved styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,15 +17,60 @@ export const Header = () => {
 
     return (
         <header className="absolute left-1/2 -translate-x-1/2 -top-10 md:-top-5 z-40 w-[85%] md:w-[80%]">
-            <div className="z-10 w-full backdrop-blur-3xl rounded-2xl px-4 md:px-8 py-1.5 md:py-1 mt-6 flex items-center justify-between border-4 border-white/40 shadow-xl">
-                <img
-                    src={Logo}
-                    alt="PokeAPI"
-                    className="h-10 md:h-14 object-contain"
-                />
-                <button onClick={setIsOpen} className="flex md:hidden">
-                    <MenuSquare />
-                </button>
+            <div
+                className={`z-10 w-full backdrop-blur-3xl rounded-2xl px-4 md:px-8 py-1.5 md:py-1 mt-6 flex items-center justify-between border-4  shadow-xl relative ${isOpen ? "bg-bgDarkGray border-white/90" : "border-white/40"}`}
+            >
+                <div className="w-full flex flex-col items-center justify-center">
+                    <div className="w-full flex justify-between">
+                        <img
+                            src={Logo}
+                            alt="PokeAPI"
+                            className="h-10 md:h-14 object-contain"
+                        />
+
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            className={`flex md:hidden p-2  hover:bg-white/20 rounded-lg transition-colors ${isOpen ? "text-white" : "text-bgDarkGray"}`}
+                        >
+                            <MenuSquare />
+                        </button>
+                    </div>
+                    {isOpen && (
+                        <div className="flex mt-2 w-full bg-white backdrop-blur-xl rounded-lg border-4 border-white/40 shadow-2xl p-4 md:hidden fadeIn duration-200 mb-3">
+                            <nav className="flex flex-col gap-3 w-full">
+                                <Link
+                                    to="/"
+                                    onClick={() => setIsOpen(false)}
+                                    className={linkStyle("/")}
+                                >
+                                    Home
+                                </Link>
+                                <Link
+                                    to="/pokemons"
+                                    onClick={() => setIsOpen(false)}
+                                    className={linkStyle("/pokemons")}
+                                >
+                                    Dex
+                                </Link>
+                                <Link
+                                    to="/compare"
+                                    onClick={() => setIsOpen(false)}
+                                    className={linkStyle("/compare")}
+                                >
+                                    Compare
+                                </Link>
+                                <Link
+                                    to="/favorites"
+                                    onClick={() => setIsOpen(false)}
+                                    className={linkStyle("/favorites")}
+                                >
+                                    Favorites
+                                </Link>
+                            </nav>
+                        </div>
+                    )}
+                </div>
+
                 <div className="hidden md:flex">
                     <nav className="flex justify-center gap-6">
                         <Link to="/" className={linkStyle("/")}>
@@ -34,10 +79,13 @@ export const Header = () => {
                         <Link to="/pokemons" className={linkStyle("/pokemons")}>
                             Dex
                         </Link>
-                        <Link to="/pokemons" className={linkStyle("/compare")}>
+                        <Link to="/compare" className={linkStyle("/compare")}>
                             Compare
                         </Link>
-                        <Link to="/pokemons" className={linkStyle("/compare")}>
+                        <Link
+                            to="/favorites"
+                            className={linkStyle("/favorites")}
+                        >
                             Favorites
                         </Link>
                     </nav>
