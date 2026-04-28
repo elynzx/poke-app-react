4eee797baa3f1c532553b0e70e3871f5646a9afd
Elynzx <rpascual06@outlook.com>
2026-04-27 22:27:56 -0500

    Refactor layout components for improved styling and responsiveness

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -26,7 +26,7 @@ export const SearchBar = ({ search, setSearch, onFilterType, onFilterGen }) => {
     };
 
     return (
-        <div className="font-item text-[12px] min-h-25 md:h-20 px-4 md:px-10 bg-bgDarkGray w-full flex items-center py-4 md:py-0">
+        <div className="font-item text-[12px] min-h-25 md:h-20 px-4 md:px-14 bg-bgDarkGray w-full flex items-center py-4 md:py-0">
             <div className="flex flex-col md:flex-row gap-3 w-full justify-between items-center">
                 <div className="flex gap-2 w-full md:w-auto h-9 md:h-10 text-white tracking-wide bg-white rounded-md items-center px-3 border-2 border-transparent focus-within:border-bgPink transition-all">
                     <Search className="text-bgDarkGray shrink-0" size={18} />
